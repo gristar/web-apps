5caaf9329c06bd762a4d5d527f1ce71947038851
chensheng <sheng.fighting@qq.com>
2016-08-20 10:20:43 +0800

    增加上传文件实例

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var app = express();
+var cookieParser = require("cookie-parser");
+
+app.use(express.static('public'));
+app.use(cookieParser());
 
 /*app.get("/", function(req,res){
 	res.end("get hello");
@@ -7,10 +11,31 @@ var app = express();
 app.post("/", function(req,res){
 	res.end("post hello");
 })*/
+app.post("*", function(req, res) {
 
-app.use(express.static('public'));
+	console.log("cookies:" + req.cookies);
+})
+app.post("/file-upload", function(req, res) {
+	console.log(req,files[0]);
+	var des_file =  __dirname + "/" + req.file[0].originalname;
+	fs.readFile(req.files[0].path,function(err,data){
+		fs.writeFile(des_file,data,function(err,data){
+			if(err){
+				console.log(err);
+			}
+			else{
+				response = {
+					msg:"上传成功",
+					filename:req.files[0].originName
+				}
+			}
+			console.log(response);
+			res.end(JSON.stringify(response));
+		})
+	})
+})
 
-var server = app.listen(2333, function(){
+var server = app.listen(2333, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log("server start,%s:%s", host, port);
